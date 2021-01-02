a2883b5ddcc8abb969dd455df73c7c26b79750a6
monkey0722 <ta19028@gmail.com>
2021-01-02 12:58:15 +0900

    Initial Commit

---
+++ Shared/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  Shared
+//
+//  Created by Taku Kobachi on 2021/01/02.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
