ba04257f0ab5afa208aa564cab4478675b3ad73b
monkey0722 <ta19028@gmail.com>
2021-01-21 22:18:48 +0900

    feat: Create a SearchBar

---
+++ iOS/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  BuildNetflix (iOS)
+//
+//  Created by Taku Kobachi on 2021/01/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let graySearchBackground = Color("gray_search_background")
+  static let graySearchText = Color("gray_search_text")
+}
